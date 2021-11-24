02eeb506776cd136bc0a6ada93eff61c0aba43a6
beatricemagana <beemag06@gmail.com>
2021-11-24 13:22:01 -0800

    add functions outline

---
+++ app.js
@@ -0,0 +1,31 @@
+var app = new function() {
+    this.el = document.getElementById('tasks');
+    this.tasks = []; 
+
+    this.FetchAll = function() {
+      
+
+    };
+
+    this.Add = function() {
+
+    };
+
+    this.Edit = function(item) {
+
+    };
+
+    this.Delete = function(item) {
+
+    };
+
+    this.Count = function(data) {
+
+    };
+}
+
+app.FetchAll();
+
+function CloseInput(){
+    document.getElementById('edit-box').style.display = 'none';
+};
