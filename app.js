e16c1ae33496455875734c53813605bb5edf5235
beatricemagana <beemag06@gmail.com>
2021-11-24 13:31:19 -0800

    add fetchAll function to edit and delete tasks from list

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ var app = new function() {
     this.tasks = []; 
 
     this.FetchAll = function() {
-      
+        var data = '';
+
+    if (this.tasks.length > 0) {
+      for (i = 0; i < this.tasks.length; i++) {
+        data += '<tr>';
+        data += '<td>'+(i+1)+". " + this.tasks[i] + '</td>';
+        data += '<td><button onclick="app.Edit(' + i + ')"  class="btn btn-warning">Edit</button></td>';
+        data += '<td><button onclick="app.Delete(' + i + ')"  class="btn btn-danger">Delete</button></td>';
+        data += '</tr>';
+      }
+    }
 
     };
 
